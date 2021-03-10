4a4fa1f34b9ab4a2589336b6b5d0305ec2f75733
Greg Parker <gregjp91@gmail.com>
2021-03-10 01:17:11 -0500

    Filtering part done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+
+var form = d3.select("form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime")
+
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    console.log(filteredData);
+}
