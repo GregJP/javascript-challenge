7c55909b325c9af0478bf76a246d8ae7da22d87c
Greg Parker <gregjp91@gmail.com>
2021-03-10 01:59:04 -0500

    Table appending done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,15 +8,22 @@ var form = d3.select("form");
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
+var filterTable = d3.select("#ufo-table")
+
 function runEnter() {
 
     d3.event.preventDefault();
 
-    var inputElement = d3.select("#datetime")
+    var inputElement = d3.select("#datetime");
 
     var inputValue = inputElement.property("value");
 
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 
-    console.log(filteredData);
-}
+    filteredData.forEach(function(filterAppend) {
+        var row = filterTable.append("tr");
+        Object.entries(filterAppend).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+});}
